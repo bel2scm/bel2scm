9b76339ee5cbb5c78a8e77b5c92a5a195e71cb2c
Craig Bakker <craig.bakker@pnnl.gov>
2020-05-18 13:45:18 -0700

    Readme file updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bel2scm", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Craig Bakker",
     author_email="craig.bakker@pnnl.gov",
     description="A package for creating causal graphs in pyro",

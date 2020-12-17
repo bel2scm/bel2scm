d305e43a5ca5bd221f8744c9789c9a8f02b48144
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-17 21:48:43 +0100

    Switch to src/ layout

--- setup.py
+++ setup.py
@@ -1,30 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Setup module."""
+
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="bel2scm", # Replace with your own username
-    version="0.0.4",
-    author="Craig Bakker",
-    author_email="craig.bakker@pnnl.gov",
-    description="A package for creating causal graphs in pyro",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/COVID-19-Causal-Reasoning/bel2scm",
-    packages=setuptools.find_packages(),
-    install_requires=[
-    	'numpy',
-    	'torch',
-    	'pyro-ppl',
-    	'networkx',
-    	'scipy',
-    	'pybel'
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+if __name__ == '__main__':
+    setuptools.setup()

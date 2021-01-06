f0cb3b910713a8ba781fdbc9c5617639bd8dc4bd
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-06 01:17:54 +0100

    Add properties

--- src/bel2scm/probability_dsl.py
+++ src/bel2scm/probability_dsl.py
@@ -19,10 +19,29 @@ class Variable(dict):
             d['index'] = index
         super().__init__(d)
 
+    @property
+    def name(self) -> str:
+        return self['name']
+
+    @property
+    def index(self) -> Optional[int]:
+        return self.get('index')
+
     def to_latex(self) -> str:
-        if 'index' in self:
-            return f'{self["name"]}_{self["index"]}'
-        return self["name"]
+        if self.index:
+            return f'{self.name}_{self.index}'
+        return self.name
+
+    def given(self, parents: Union[str, 'Variable', List[Union[str, 'Variable']]]) -> 'Condition':
+        if isinstance(parents, (str, Variable)):
+            parents = [parents]
+        return Condition(
+            child=self,
+            parents=parents,
+        )
+
+    def __ror__(self, parents: Union[str, 'Variable', List[Union[str, 'Variable']]]) -> 'Condition':
+        return self.given(parents)
 
 
 class Condition(dict):
@@ -38,17 +57,40 @@ class Condition(dict):
             'parents': parents,
         })
 
+    @property
+    def child(self) -> Variable:
+        return self['child']
+
+    @property
+    def parents(self) -> List[Variable]:
+        return self['parents']
+
     def to_latex(self) -> str:
-        parents = ','.join(p.to_latex() for p in self['parents'])
-        return f'{self["child"].to_latex()}|{parents}'
+        parents = ','.join(p.to_latex() for p in self.parents)
+        return f'{self.child.to_latex()}|{parents}'
+
+    def given(self, parents: Union[str, 'Variable', List[Union[str, 'Variable']]]) -> 'Condition':
+        if isinstance(parents, (str, Variable)):
+            parents = [parents]
+        return Condition(
+            child=self.child,
+            parents=[*self.parents, *parents]
+        )
+
+    def __ror__(self, parents: Union[str, 'Variable', List[Union[str, 'Variable']]]) -> 'Condition':
+        return self.given(parents)
 
 
 class Probability(dict):
     def __init__(self, probability: Condition):
         super().__init__({'probability': probability})
 
+    @property
+    def probability(self) -> Condition:
+        return self["probability"]
+
     def to_latex(self) -> str:
-        return f'P({self["probability"].to_latex()})'
+        return f'P({self.probability.to_latex()})'
 
 
 class Sum(dict):
@@ -60,9 +102,15 @@ class Sum(dict):
             },
         })
 
+    def ranges(self) -> List[Variable]:
+        return self['sum']['ranges']
+
+    def expressions(self) -> List[Union[Probability, 'Expression']]:
+        return self['sum']['expressions']
+
     def to_latex(self) -> str:
-        ranges = ','.join(r.to_latex() for r in self['sum']['ranges'])
-        summands = ' '.join(e.to_latex() for e in self['sum']['expressions'])
+        ranges = ','.join(r.to_latex() for r in self.ranges)
+        summands = ' '.join(e.to_latex() for e in self.expressions())
         return f'[ sum_{{{ranges}}} {summands} ]'
 
 
@@ -75,8 +123,15 @@ class Frac(dict):
             },
         })
 
+    @property
+    def numerator(self) -> List['Expression']:
+        return self['frac']['numerator']
+
+    def denominator(self) -> List['Expression']:
+        return self['frac']['denominator']
+
     def to_latex(self) -> str:
-        return f"{self['frac']['numerator']} / {self['frac']['denominator']}"
+        return f"{self.numerator} / {self.denominator()}"
 
 
 Expression = Union[Condition, Sum, Frac]
